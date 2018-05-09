10b5a564a618f37dedbd3db6c21070b864bbe05a
vandana-ncess <38155285+vandana-ncess@users.noreply.github.com>
2018-05-09 15:27:25 +0530

    gallery
    gallery

--- test.php
+++ test.php
@@ -9,7 +9,7 @@
     <!-- #region Jssor Slider Begin -->
     <!-- Generator: Jssor Slider Maker -->
     <!-- Source: https://www.jssor.com -->
-    <script src="/js/jssor.slider.min.js" type="text/javascript"></script>
+    <script src="../js/jssor.slider.min.js" type="text/javascript"></script>
     <script type="text/javascript">
         jssor_1_slider_init = function() {
 
@@ -96,65 +96,16 @@
         </div>
         <div data-u="slides" style="cursor:default;position:relative;top:0px;left:0px;width:980px;height:150px;overflow:hidden;">
             <div>
-                <img data-u="image" src="../img/gallery/200x150/001.jpg" />
+                 <?php  for($i=1;$i<=20;$i++) {
+                          echo '<div>
+                <img src="../img/gallery/200x150/'+ $i+'.jpg" height="50px" width="70px" /></div>' ;
+                        } ?>
+           
             </div>
-            <div>
-                <img data-u="image" src="../img/gallery/200x150/005.jpg" />
-            </div>
-            <div>
-                <img data-u="image" src="../img/gallery/200x150/006.jpg" />
-            </div>
-            <div>
-                <img data-u="image" src="../img/gallery/200x150/007.jpg" />
-            </div>
-            <div>
-                <img data-u="image" src="../img/gallery/200x150/008.jpg" />
-            </div>
-            <div>
-                <img data-u="image" src="../img/gallery/200x150/009.jpg" />
-            </div>
-            <div>
-                <img data-u="image" src="../img/gallery/200x150/010.jpg" />
-            </div>
-            <div>
-                <img data-u="image" src="../img/gallery/200x150/011.jpg" />
-            </div>
-            <div>
-                <img data-u="image" src="../img/gallery/200x150/024.jpg" />
-            </div>
-            <div>
-                <img data-u="image" src="../img/gallery/200x150/025.jpg" />
-            </div>
-            <div>
-                <img data-u="image" src="../img/gallery/200x150/026.jpg" />
-            </div>
-            <div>
-                <img data-u="image" src="../img/gallery/200x150/027.jpg" />
-            </div>            <div style="background-color:#ff7c28;">
-                <div style="position:absolute;top:3px;left:16px;width:150px;height:62px;z-index:0;font-size:16px;color:#000000;line-height:24px;text-align:left;padding:5px;box-sizing:border-box;">Photos in this slider are to demostrate jssor slider,<br />
-                    which are not licensed for any other purpose.
-                </div>
-            </div>
+           
         </div>
         <!-- Bullet Navigator -->
-        <div data-u="navigator" class="jssorb057" style="position:absolute;bottom:12px;right:12px;" data-autocenter="1" data-scale="0.5" data-scale-bottom="0.75">
-            <div data-u="prototype" class="i" style="width:16px;height:16px;">
-                <svg viewBox="0 0 16000 16000" style="position:absolute;top:0;left:0;width:100%;height:100%;">
-                    <circle class="b" cx="8000" cy="8000" r="5000"></circle>
-                </svg>
-            </div>
-        </div>
-        <!-- Arrow Navigator -->
-        <div data-u="arrowleft" class="jssora073" style="width:50px;height:50px;top:0px;left:30px;" data-autocenter="2" data-scale="0.75" data-scale-left="0.75">
-            <svg viewBox="0 0 16000 16000" style="position:absolute;top:0;left:0;width:100%;height:100%;">
-                <path class="a" d="M4037.7,8357.3l5891.8,5891.8c100.6,100.6,219.7,150.9,357.3,150.9s256.7-50.3,357.3-150.9 l1318.1-1318.1c100.6-100.6,150.9-219.7,150.9-357.3c0-137.6-50.3-256.7-150.9-357.3L7745.9,8000l4216.4-4216.4 c100.6-100.6,150.9-219.7,150.9-357.3c0-137.6-50.3-256.7-150.9-357.3l-1318.1-1318.1c-100.6-100.6-219.7-150.9-357.3-150.9 s-256.7,50.3-357.3,150.9L4037.7,7642.7c-100.6,100.6-150.9,219.7-150.9,357.3C3886.8,8137.6,3937.1,8256.7,4037.7,8357.3 L4037.7,8357.3z"></path>
-            </svg>
-        </div>
-        <div data-u="arrowright" class="jssora073" style="width:50px;height:50px;top:0px;right:30px;" data-autocenter="2" data-scale="0.75" data-scale-right="0.75">
-            <svg viewBox="0 0 16000 16000" style="position:absolute;top:0;left:0;width:100%;height:100%;">
-                <path class="a" d="M11962.3,8357.3l-5891.8,5891.8c-100.6,100.6-219.7,150.9-357.3,150.9s-256.7-50.3-357.3-150.9 L4037.7,12931c-100.6-100.6-150.9-219.7-150.9-357.3c0-137.6,50.3-256.7,150.9-357.3L8254.1,8000L4037.7,3783.6 c-100.6-100.6-150.9-219.7-150.9-357.3c0-137.6,50.3-256.7,150.9-357.3l1318.1-1318.1c100.6-100.6,219.7-150.9,357.3-150.9 s256.7,50.3,357.3,150.9l5891.8,5891.8c100.6,100.6,150.9,219.7,150.9,357.3C12113.2,8137.6,12062.9,8256.7,11962.3,8357.3 L11962.3,8357.3z"></path>
-            </svg>
-        </div>
+       
     </div>
     <script type="text/javascript">jssor_1_slider_init();</script>
     <!-- #endregion Jssor Slider End -->
